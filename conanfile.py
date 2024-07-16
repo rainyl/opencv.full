67b93240201e006eba74e49197c352de17d8d3c4
rainy liu <rainyliusy3@gmail.com>
2024-07-16 20:03:04 +0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -386,8 +386,8 @@ class OcvDartDesktop(ConanFile):
         # build ffmpeg only for linux, on macos, install with brew,
         # on windows use prebuilt dll
         # android and ios are unsupported
-        if self.settings.os == "Linux":
-            self.tool_requires("ffmpeg/6.1")
+        # if self.settings.os == "Linux":
+        #     self.tool_requires("ffmpeg/6.1")
         if self.settings.os != "Windows":
             self.tool_requires("ninja/1.11.1")
 
